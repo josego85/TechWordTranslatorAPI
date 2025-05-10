88819ef215dff3738102b3ee013f630b82e29297
josego <josego85@gmail.com>
2025-05-10 12:15:47 +0200

    feat: add `CursorPaginationLinks` trait to DRY up pagination link generation

---
+++ app/Http/Resources/Traits/CursorPaginationLinks.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Pagination\CursorPaginator;
+
+/**
+ * Build next/prev links for a CursorPaginator.
+ */
+trait CursorPaginationLinks
+{
+    /**
+     * @param  Request         $request
+     * @param  CursorPaginator $paginator
+     * @return array{next: string|null, prev: string|null}
+     */
+    protected function buildCursorLinks(Request $request, CursorPaginator $paginator): array
+    {
+        $links = [];
+        $map = [
+            'next' => 'nextCursor',
+            'prev' => 'previousCursor',
+        ];
+
+        foreach ($map as $key => $method) {
+            $cursor = $paginator->{$method}()?->encode();
+
+            $links[$key] = $cursor
+                ? $request->fullUrlWithQuery(['cursor' => $cursor])
+                : null;
+        }
+
+        return $links;
+    }
+}
