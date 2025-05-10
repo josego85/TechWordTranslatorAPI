6c7cbc400e9f76ceef5a7c36f4a60d587c37d10f
josego <josego85@gmail.com>
2025-05-10 13:16:22 +0200

    feat: API versioning: all routes now prefixed with `/api/v1`

--- app/Http/Controllers/API/V1/WordController.php
+++ app/Http/Controllers/API/V1/WordController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\WordIndexRequest;
 use App\Http\Resources\WordCollection;
 use App\Http\Resources\WordResource;
--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WordController;
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\V1\WordController;
+use App\Http\Controllers\API\V1\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('v1')
+  ->name('api.v1.')
+  ->middleware('api')
+  ->group(function () {
+        Route::prefix('/user')
+            ->name('user.')
+            ->group(function () {
+                Route::post('register', [AuthController::class, 'register'])->name('register');
+                Route::post('login',    [AuthController::class, 'login'])->name('login');
+                Route::get('/',         [AuthController::class, 'getUser'])->name('getUser');
+            });
 
-Route::prefix('/user')->name('user.')->group(function () {
-    Route::post('register', [AuthController::class, 'register'])->name('register');
-    Route::post('login', [AuthController::class, 'login'])->name('login');
-    Route::get('/', [AuthController::class, 'getUser'])->name('getUser');
+        Route::middleware('jwt.verify')->group(function () {
+            Route::apiResource('words', WordController::class)
+                ->only('store', 'update', 'destroy');
+        });
+   
+        Route::apiResource('words', WordController::class)
+            ->only('index', 'show');
 });
-
-Route::middleware('jwt.verify')->group(function () {
-    Route::apiResource('words', WordController::class)->only('store', 'update', 'destroy');
-});
-
-Route::apiResource('words', WordController::class)->only('index', 'show');
